c3fe7e48f947cef54bd28b110ca1a13e32bc61c1
pepenacho <herreragrej@proton.me>
2024-10-25 19:31:19 -0300

    create client.sql

---
+++ db/tables/client.sql
@@ -0,0 +1,16 @@
+CREATE TYPE payment_method AS ENUM ('credit_card', 'debit_card', 'paypal', 'bank_transfer', 'cash');
+CREATE TYPE customer_segment AS ENUM ('VIP', 'frequent', 'occasional', 'new');
+
+CREATE TABLE clients (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    person_id BIGINT PRIMARY KEY REFERENCES person(id) ON DELETE CASCADE,
+    loyalty_points INT NOT NULL CHECK (loyalty_points >= 0),
+    preferred_payment_method payment_method,
+    customer_segment customer_segment,
+    last_purchase_date TIMESTAMP WITH TIME ZONE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    update_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+)
+
+CREATE INDEX idx_customer_segment ON clients(customer_segment);
+CREATE INDEX idx_last_purchase_date ON clients(last_purchase_date);
