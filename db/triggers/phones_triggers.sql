b99caa9826701ab1964e568e74ead464365c4c4f
pepenacho <herreragrej@proton.me>
2024-10-25 17:16:19 -0300

    create phones_triggers.sql

--- db/triggers/phones_triggers.sql
+++ db/triggers/phones_triggers.sql
@@ -2,3 +2,8 @@ CREATE TRIGGER phone_presence_trigger
 AFTER INSERT ON person
 FOR EACH ROW
 EXECUTE FUNCTION check_phone_before_insert();
+
+CREATE TRIGGER validate_phone_trigger
+AFTER INSERT ON phones
+FOR EACH ROW
+EXECUTE FUNCTION validate_phone();
