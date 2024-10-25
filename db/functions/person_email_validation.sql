3cc5cd04321e8e0d995af998122ea686b382d9fb
pepenacho <herreragrej@proton.me>
2024-10-25 14:42:48 -0300

    update person_email_validation.sql

--- db/functions/person_email_validation.sql
+++ db/functions/person_email_validation.sql
@@ -1,17 +1,17 @@
 CREATE OR REPLACE FUNCTION validate_email_format(email TEXT)
 RETURNS BOOLEAN AS $$
 BEGIN
-    RETURN email ~ '^[a-zA-Z0-9!#$%&''*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&''*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$';
+    RETURN email ~ '^[a-zA-Z0-9!#$%&''*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&''*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$';
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION check_email_uniqueness(email TEXT, user_id BIGINT DEFAULT NULL)
+CREATE OR REPLACE FUNCTION check_email_uniqueness(new_email TEXT, user_id BIGINT DEFAULT NULL)
 RETURNS BOOLEAN ASS $$
 BEGIN
     RETURN NOT EXISTS (
         SELECT 1
-        FROM "Users"
-        WHERE email = email AND (user_id IS NULL OR id != user_id)
+        FROM person
+        WHERE email = new_email AND (user_id IS NULL OR id != user_id)
     );
 END;
 $$ LANGUAGE plpgsql;
