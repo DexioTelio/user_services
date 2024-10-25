a7156205cf8d856ae694a16d7d14748cb0273583
pepenacho <herreragrej@proton.me>
2024-10-25 13:45:36 -0300

    update address.sql

--- db/tables/address.sql
+++ db/tables/address.sql
@@ -1,6 +1,6 @@
 -- Creación de la tabla "Address"
 CREATE TABLE "Address" (
-    user_id BIGINT NOT NULL REFERENCES "Users" (id) ON DELETE CASCADE,
+    person_id BIGINT NOT NULL REFERENCES "Users" (id) ON DELETE CASCADE,
     street VARCHAR(255) NOT NULL,
     street_number VARCHAR(20) NOT NULL,
     apartment_number VARCHAR(10),
@@ -17,4 +17,5 @@ CREATE TABLE "Address" (
 CREATE INDEX idx_address_city ON "Address" USING btree (city);
 CREATE INDEX idx_address_state ON "Address" USING btree (state);
 CREATE INDEX idx_address_country ON "Address" USING btree (country);
-CREATE INDEX idx_address ON "Address" USING btree (street, street_number, neighborhood, city, state, postal_code, country);
+CREATE INDEX idx_address ON "Address" USING btree
+    (street, street_number, neighborhood, city, state, postal_code, country);
