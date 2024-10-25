a437cc75f68f2c6d295704a7f712f135b338e004
pepenacho <herreragrej@proton.me>
2024-10-25 17:15:44 -0300

    create phones_functions.sql

--- db/functions/phones_functions.sql
+++ db/functions/phones_functions.sql
@@ -1,8 +1,41 @@
 CREATE OR REPLACE FUNCTION check_phone_before_insert()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF (SELECT COUNT(*) FROM phones WHERE person_id = NEW.id) THEN
+    IF (SELECT COUNT(*) FROM phones WHERE person_id = NEW.person_id) = 0 THEN
         RAISE EXCEPT 'The user must have at least one telephone number.'
     END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION validate_phone_format(phone_number TEXT)
+RETURNS BOOLEAN AS $$
+BEGIN
+    RETURN phone_number ~ '^\\+?(\\d{1,3})?[-.\\s]?(\\(?\\d{1,4}?\\)?)[-.\\s]?(\\d{1,4})[-.\\s]?(\\d{1,4})[-.\\s]?(\\d{1,9})$'
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION validate_phone_uniqueness(new_phone_number TEXT, person_id BIGINT)
+RETURN BOOLEAN AS $$
+BEGIN
+    RETURN NOT EXISTS (
+        SELECT 1
+        FROM phones
+        WHERE phone_number = new_phone_number AND (person_id IS NULL OR phones.person_id != person_id)
+    );
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION validate_phone()
+RETURN TRIGGER AS $$
+BEGIN
+    IF NOT validate_phone_format(NEW.phone_number) THEN
+        RAISE EXCEPTION 'Invalid phone number format.'
+    END IF;
+
+    IF NOT validate_phone_uniqueness(NEW.phone_number, NEW.person_id) THEN
+        RAISE EXCEPTION 'Phone number already in use.'
+    END IF;
+
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
