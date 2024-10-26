e9293577e52f4f835766799c75a6e3ebe736fa18
pepenacho <herreragrej@proton.me>
2024-10-26 02:17:58 -0300

    syntax corrections

--- db/tables/client.sql
+++ db/tables/client.sql
@@ -3,14 +3,14 @@ CREATE TYPE customer_segment AS ENUM ('VIP', 'frequent', 'occasional', 'new');
 
 CREATE TABLE clients (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    person_id BIGINT PRIMARY KEY REFERENCES person(id) ON DELETE CASCADE,
+    person_id BIGINT REFERENCES person(id) ON DELETE CASCADE,
     loyalty_points INT NOT NULL CHECK (loyalty_points >= 0),
     preferred_payment_method payment_method,
     customer_segment customer_segment,
     last_purchase_date TIMESTAMP WITH TIME ZONE,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     update_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-)
+);
 
 CREATE INDEX idx_customer_segment ON clients(customer_segment);
 CREATE INDEX idx_last_purchase_date ON clients(last_purchase_date);
