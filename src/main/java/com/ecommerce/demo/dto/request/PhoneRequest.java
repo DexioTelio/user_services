a1ff39c0540e2f80342640970f155ea398a5b106
pepenacho <herreragrej@proton.me>
2024-11-06 00:49:49 -0300

    crate PhoneRequest

--- src/main/java/com/ecommerce/demo/dto/request/PhoneRequest.java
+++ src/main/java/com/ecommerce/demo/dto/request/PhoneRequest.java
@@ -3,11 +3,23 @@ package com.ecommerce.demo.dto.request;
 import com.ecommerce.demo.enums.PhoneType;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public class PhoneRequest {
+    @NotNull(message = "ID is required")
     private final Long id;
+
+    @NotNull(message = "Person ID is required")
     private final Long personId;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\+?(\\d{1,3})?[-.\\s]?(\\(?\\d{1,4}?\\)?)[-.\\s]?(\\d{1,4})[-.\\s]?(\\d{1,4})[-.\\s]?(\\d{1,9})$",
+            message = "Phone number format is invalid")
     private final String phoneNumber;
+
+    @NotNull(message = "Phone type is required")
     private final PhoneType phoneType;
 
     @JsonCreator
