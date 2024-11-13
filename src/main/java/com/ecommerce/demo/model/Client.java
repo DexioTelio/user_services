842eec54f8bd18abf53bae5fa938669da49f3a26
pepenacho <herreragrej@proton.me>
2024-11-13 00:42:58 -0300

    change from user to client

---
+++ src/main/java/com/ecommerce/demo/model/Client.java
@@ -0,0 +1,54 @@
+package com.ecommerce.demo.model;
+
+import com.ecommerce.demo.enums.CustomerSegment;
+import com.ecommerce.demo.enums.Gender;
+import com.ecommerce.demo.enums.PreferredPaymentMethod;
+import com.ecommerce.demo.enums.Role;
+
+import java.time.ZonedDateTime;
+
+import java.util.Set;
+
+public class Client {
+    private final Long id;
+    private final Long personId;
+    private final int loyaltyPoints;
+    private final PreferredPaymentMethod preferredPaymentMethod;
+    private final CustomerSegment customerSegment;
+    private final ZonedDateTime lastPurchaseDate;
+    private final ZonedDateTime createdAt;
+    private final ZonedDateTime updatedAt;
+
+    public Client(Builder builder) {
+        this.id = builder.id;
+        this.personId = builder.personId;
+        this.loyaltyPoints = builder.loyaltyPoints;
+        this.preferredPaymentMethod = builder.preferredPaymentMethod;
+        this.customerSegment = builder.customerSegment;
+        this.lastPurchaseDate = builder.lastPurchaseDate;
+        this.createdAt = builder.createdAt;
+        this.updatedAt = builder.updatedAt;
+    }
+
+    public static class Builder {
+        private Long id;
+        private Long personId;
+        private int loyaltyPoints;
+        private PreferredPaymentMethod preferredPaymentMethod;
+        private CustomerSegment customerSegment;
+        private ZonedDateTime lastPurchaseDate;
+        private ZonedDateTime createdAt;
+        private ZonedDateTime updatedAt;
+
+        public Builder id(Long id) { this.id = id; return this; }
+        public Builder personId(Long personId) { this.personId = personId; return this; }
+        public Builder loyaltyPoints(int loyaltyPoints) { this.loyaltyPoints = loyaltyPoints; return this; }
+        public Builder preferredPaymentMethod(PreferredPaymentMethod preferredPaymentMethod) { this.preferredPaymentMethod = preferredPaymentMethod; return this; }
+        public Builder customerSegment(CustomerSegment customerSegment) { this.customerSegment = customerSegment; return this; }
+        public Builder lastPurchaseDate(ZonedDateTime lastPurchaseDate) { this.lastPurchaseDate = lastPurchaseDate; return this; }
+
+        public Client build() {
+            return new Client(this);
+        }
+    }
+}
