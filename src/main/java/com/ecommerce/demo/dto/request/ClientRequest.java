c352c5a6d60f66ef8bc17539ae8b873c89e30520
pepenacho <herreragrej@proton.me>
2024-11-13 03:13:45 -0300

    update ClientRequest

--- src/main/java/com/ecommerce/demo/dto/request/ClientRequest.java
+++ src/main/java/com/ecommerce/demo/dto/request/ClientRequest.java
@@ -2,29 +2,32 @@ package com.ecommerce.demo.dto.request;
 
 import com.ecommerce.demo.enums.CustomerSegment;
 import com.ecommerce.demo.enums.PreferredPaymentMethod;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.ZonedDateTime;
 
-public class ClientRequest {
-    private final Long personId;
-    private final int loyaltyPoints;
-    private final PreferredPaymentMethod preferredPaymentMethod;
-    private final CustomerSegment customerSegment;
-    private final ZonedDateTime lastPurchaseDate;
+public record ClientRequest(
+        @NotNull(message = "Id is required")
+        @JsonProperty("id") Long id,
 
-    @JsonCreator
-    public ClientRequest(
-            @JsonProperty("personId") Long personId,
-            @JsonProperty("loyaltyPoints") int loyaltyPoints,
-            @JsonProperty("preferredPaymentMethod") PreferredPaymentMethod preferredPaymentMethod,
-            @JsonProperty("customerSegment") CustomerSegment customerSegment,
-            @JsonProperty("lastPurchaseDate") ZonedDateTime lastPurchaseDate) {
-        this.personId = personId;
-        this.loyaltyPoints = loyaltyPoints;
-        this.preferredPaymentMethod = preferredPaymentMethod;
-        this.customerSegment = customerSegment;
-        this.lastPurchaseDate = lastPurchaseDate;
-    }
-}
+        @NotNull(message = "Person id is required")
+        @JsonProperty("person_id") Long personId,
+
+        @NotNull(message = "Loyalty points is required")
+        @Min(value = 0, message = "The loyalty points cannot be negative")
+        @JsonProperty("loyalty_points") int loyaltyPoints,
+
+        @NotNull(message = "Preferred payment method is required")
+        @JsonProperty("Preferred_payment_method") PreferredPaymentMethod preferredPaymentMethod,
+
+        @NotNull(message = "Customer segment is required")
+        @JsonProperty("customer_segment") CustomerSegment customerSegment,
+
+        @NotNull(message = "Last purchase date is required")
+        @PastOrPresent(message = "Last purchase date must e a past or present date")
+        @JsonProperty("last_purchase_date") ZonedDateTime lastPurchaseDate
+) {}
+
