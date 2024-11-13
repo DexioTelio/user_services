89dbb3f0bd1a13635d6bad2fd6f9a39bc2ed6043
pepenacho <herreragrej@proton.me>
2024-11-12 23:29:06 -0300

    update PersonWriteServices

--- src/main/java/com/ecommerce/demo/repositories/interfaces/PersonQueryRepository.java
+++ src/main/java/com/ecommerce/demo/repositories/interfaces/PersonQueryRepository.java
@@ -4,9 +4,7 @@ import com.ecommerce.demo.model.User;
 import com.ecommerce.demo.util.Result;
 import io.vavr.control.Try;
 
-public interface UserQueryRepository {
+public interface PersonQueryRepository {
     User findById(Long id);
     Try<Result<Boolean>> exists(String email);
-    Try<Result<Boolean>> existsUsername(String username);
-    Try<Result<User>> findByUsername(String username);
 }
