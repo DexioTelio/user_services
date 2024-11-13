8f010ab1a8e0a2f5657ebf6a8a00d0c924b85b6b
pepenacho <herreragrej@proton.me>
2024-11-12 23:36:45 -0300

    update PersonWriteRepository

--- src/main/java/com/ecommerce/demo/repositories/interfaces/PersonWriteRepository.java
+++ src/main/java/com/ecommerce/demo/repositories/interfaces/PersonWriteRepository.java
@@ -1,10 +1,11 @@
 package com.ecommerce.demo.repositories.interfaces;
 
+import com.ecommerce.demo.model.Person;
 import com.ecommerce.demo.model.User;
 import com.ecommerce.demo.util.Result;
 
-public interface UserWriteRepository {
-    Result<Long> create(User user);
-    void update(User user);
+public interface PersonWriteRepository {
+    Result<Long> create(Person person);
+    void update(Person person);
     void delete(Long id);
 }
