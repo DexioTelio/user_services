a59c13fd6813cbb12863348210de760eeb012545
pepenacho <herreragrej@proton.me>
2024-11-23 01:37:49 -0300

    Created PersonResponse

--- src/main/java/com/ecommerce/demo/dto/response/PersonResponse.java
+++ src/main/java/com/ecommerce/demo/dto/response/PersonResponse.java
@@ -1,25 +1,22 @@
 package com.ecommerce.demo.dto.response;
 
-import com.ecommerce.demo.model.User;
 import com.ecommerce.demo.enums.Gender;
 
 import java.time.ZonedDateTime;
 import java.util.Set;
 
-public class UserResponse {
+public class PersonResponse {
     private final String firstName;
     private final String lastName;
-    private final String userName;
     private final String email;
     private final Set<String> phones;
     private final Gender gender;
     private final ZonedDateTime createdAt;
     private final ZonedDateTime updatedAt;
 
-    private UserResponse(Builder builder) {
+    private PersonResponse(Builder builder) {
         this.firstName = builder.firstName;
         this.lastName = builder.lastName;
-        this.userName = builder.userName;
         this.email = builder.email;
         this.phones = builder.phones;
         this.gender = builder.gender;
@@ -30,7 +27,6 @@ public class UserResponse {
     public static class Builder {
         private String firstName;
         private String lastName;
-        private String userName;
         private String email;
         private Set<String> phones;
         private Gender gender;
@@ -39,47 +35,30 @@ public class UserResponse {
 
         public Builder firstName(String firstName) { this.firstName = firstName; return this; }
         public Builder lastName(String lastName) { this.lastName = lastName; return this; }
-        public Builder userName(String userName) { this.userName = userName; return this; }
         public Builder email(String email) { this.email = email; return this; }
         public Builder phones(Set<String> phones) { this.phones = phones; return this; }
         public Builder gender(Gender gender) { this.gender = gender; return this; }
         public Builder createdAt(ZonedDateTime createdAt) { this.createdAt = createdAt; return this; }
         public Builder updatedAt(ZonedDateTime updatedAt) { this.updatedAt = updatedAt; return this; }
 
-        public UserResponse build() {
-            return new UserResponse(this);
+        public PersonResponse build() {
+            return new PersonResponse(this);
         }
-    }
-
-    public static UserResponse toUserResponse(User user) {
-        return new Builder()
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
-                .userName(user.getUserName())
-                .email(user.getEmail())
-                .phones(user.getPhones())
-                .gender(user.getGender())
-                .createdAt(user.getCreatedAt())
-                .updatedAt(user.getUpdatedAt())
-                .build();
     }
 
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
-    public String getUserName() { return userName; }
     public String getEmail() { return email; }
     public Set<String> getPhones() { return phones; }
     public Gender getGender() { return gender; }
     public ZonedDateTime getCreatedAt() { return createdAt; }
     public ZonedDateTime getUpdatedAt() { return updatedAt; }
 
-
     @Override
     public String toString() {
-        return "UserResponse{" +
+        return "PersonResponse{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", userName='" + userName + '\'' +
                 ", email='" + email + '\'' +
                 ", phones=" + phones +
                 ", gender=" + gender +
