5b7f42959e3fc9c76c885cbf24ac1bcd235c7476
pepenacho <herreragrej@proton.me>
2024-11-24 04:21:04 -0300

    update AddressWriteServices

--- src/main/java/com/ecommerce/demo/services/interfaces/AddressWriteServices.java
+++ src/main/java/com/ecommerce/demo/services/interfaces/AddressWriteServices.java
@@ -5,7 +5,7 @@ import com.ecommerce.demo.dto.response.AddressResponse;
 import com.ecommerce.demo.util.Result;
 
 public interface AddressWriteServices {
-    Result<AddressResponse> create(Long userId, AddressRequest request);
+    Result<Void> create(Long userId, AddressRequest request);
     Result<AddressResponse> update(AddressRequest request);
     Result<AddressResponse>  delete(AddressRequest request);
 }
