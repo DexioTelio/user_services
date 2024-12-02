0b5677137501350f8d43dbe6fb1882fdc3f774a0
pepenacho <herreragrej@proton.me>
2024-12-02 17:05:36 -0300

    update PhonesErrorCode

--- src/main/java/com/ecommerce/demo/enums/PhonesErrorCode.java
+++ src/main/java/com/ecommerce/demo/enums/PhonesErrorCode.java
@@ -1,7 +1,8 @@
 package com.ecommerce.demo.enums;
 
 public enum PhonesErrorCode {
-  PHONES_CREATION_FAILURE("The phone/s already exists.");
+  PHONES_CREATION_FAILURE("The phone/s already exists."),
+  PHONE_ERROR_CREATION("Failed to create phone");
 
   private final String message;
 
