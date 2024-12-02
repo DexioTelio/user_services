72f0934d9baa7fc37c7345039584f03a8324c02b
pepenacho <herreragrej@proton.me>
2024-12-02 12:05:04 -0300

    update PhoneRequest

--- src/main/java/com/ecommerce/demo/dto/request/PhoneRequest.java
+++ src/main/java/com/ecommerce/demo/dto/request/PhoneRequest.java
@@ -1,37 +1,23 @@
 package com.ecommerce.demo.dto.request;
 
 import com.ecommerce.demo.enums.PhoneType;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public class PhoneRequest {
+public record PhoneRequest(
     @NotNull(message = "ID is required")
-    private final Long id;
+    @JsonProperty("id") Long id,
 
     @NotNull(message = "Person ID is required")
-    private final Long personId;
+    @JsonProperty("person_id") Long personId,
 
     @NotBlank(message = "Phone number is required")
     @Pattern(regexp = "^\\+?(\\d{1,3})?[-.\\s]?(\\(?\\d{1,4}?\\)?)[-.\\s]?(\\d{1,4})[-.\\s]?(\\d{1,4})[-.\\s]?(\\d{1,9})$",
             message = "Phone number format is invalid")
-    private final String phoneNumber;
+    @JsonProperty("phone_number") String phoneNumber,
 
     @NotNull(message = "Phone type is required")
-    private final PhoneType phoneType;
-
-    @JsonCreator
-    public PhoneRequest(
-            @JsonProperty("id") Long id,
-            @JsonProperty("personId") Long personId,
-            @JsonProperty("phoneNumber") String phoneNumber,
-            @JsonProperty("phoneType") PhoneType phoneType
-    ) {
-        this.id = id;
-        this.personId = personId;
-        this.phoneNumber = phoneNumber;
-        this.phoneType = phoneType;
-    }
-}
+    @JsonProperty("phone_type") PhoneType phoneType
+) {}
