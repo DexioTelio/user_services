2dad1c7f9f0224f6ae2c28e6367b880c310a795a
pepenacho <herreragrej@proton.me>
2024-12-02 18:22:35 -0300

    update AddressWriteRepository

--- src/main/java/com/ecommerce/demo/repositories/interfaces/AddressWriteRepository.java
+++ src/main/java/com/ecommerce/demo/repositories/interfaces/AddressWriteRepository.java
@@ -1,10 +1,9 @@
 package com.ecommerce.demo.repositories.interfaces;
 
-import com.ecommerce.demo.dto.request.AddressRequest;
 import com.ecommerce.demo.model.Address;
 import com.ecommerce.demo.util.Result;
 
 public interface AddressWriteRepository {
-    Result<Void> create(Long personId, AddressRequest address);
+    Result<Void> create(Address address);
     Result<Void> delete(Long id);
 }
