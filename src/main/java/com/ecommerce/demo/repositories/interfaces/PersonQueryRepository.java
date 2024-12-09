825c295f9521ae62bc85ac8774df4629fc23011b
pepenacho <herreragrej@proton.me>
2024-12-09 10:25:32 -0300

    update PersonQueryRepository

--- src/main/java/com/ecommerce/demo/repositories/interfaces/PersonQueryRepository.java
+++ src/main/java/com/ecommerce/demo/repositories/interfaces/PersonQueryRepository.java
@@ -1,10 +1,11 @@
 package com.ecommerce.demo.repositories.interfaces;
 
-import com.ecommerce.demo.model.User;
+import com.ecommerce.demo.model.Person;
 import com.ecommerce.demo.util.Result;
 import io.vavr.control.Try;
 
 public interface PersonQueryRepository {
-    User findById(Long id);
-    Try<Result<Boolean>> exists(String email);
+  Person findById(Long id);
+  Person findByUsername(String username);
+  Try<Result<Boolean>> exists(String email);
 }
