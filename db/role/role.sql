3cf80979aa696f98412741c7254d29b3b2d31f4b
pepenacho <herreragrej@proton.me>
2024-12-09 15:14:01 -0300

    update role

--- db/role/role.sql
+++ db/role/role.sql
@@ -3,7 +3,8 @@ CREATE ROLE employee;
 CREATE ROLE admin;
 CREATE ROLE manager
 CREATE ROLE guest;
-CREATE ROLE 'customer support';
+CREATE ROLE customer_support;
 
 GRANT ALL PRIVILEGES ON DATABASE user_services TO admin;
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE clients TO client;
+GRANT SELECT, UPDATE ON TABLE clients TO client;
+GRANT SELECT, UPDATE ON TABLE persons TO client;
