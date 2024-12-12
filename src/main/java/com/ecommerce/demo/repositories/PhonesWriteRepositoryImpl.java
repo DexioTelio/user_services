e13eb292edf499a003088c81c23a4177d11bc170
pepenacho <herreragrej@proton.me>
2024-12-12 12:40:25 -0300

    update PhonesWriteRepositoryImpl

--- src/main/java/com/ecommerce/demo/repositories/PhonesWriteRepositoryImpl.java
+++ src/main/java/com/ecommerce/demo/repositories/PhonesWriteRepositoryImpl.java
@@ -9,7 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class PhonesWriteRepositoryImpl implements PhonesWriteRepository {
     private final Logger logger = LoggerFactory.getLogger(PhonesWriteRepositoryImpl.class);
 
