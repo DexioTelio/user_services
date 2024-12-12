8f163d52fdb72235b660c8804511cac940ccd808
pepenacho <herreragrej@proton.me>
2024-12-12 12:33:38 -0300

    update ClientWriteRepositoryImpl

--- src/main/java/com/ecommerce/demo/repositories/ClientWriteRepositoryImpl.java
+++ src/main/java/com/ecommerce/demo/repositories/ClientWriteRepositoryImpl.java
@@ -10,7 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ClientWriteRepositoryImpl implements ClientWriteRepository {
   private final Logger logger = LoggerFactory.getLogger(ClientWriteRepositoryImpl.class);
 
