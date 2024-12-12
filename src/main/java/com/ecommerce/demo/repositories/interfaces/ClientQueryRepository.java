2de30cbe00965ebfb3e3e19e97c6845bfa13313a
pepenacho <herreragrej@proton.me>
2024-12-11 22:20:02 -0300

    update ClientQueryRepository

--- src/main/java/com/ecommerce/demo/repositories/interfaces/ClientQueryRepository.java
+++ src/main/java/com/ecommerce/demo/repositories/interfaces/ClientQueryRepository.java
@@ -1,11 +1,10 @@
 package com.ecommerce.demo.repositories.interfaces;
 
 import com.ecommerce.demo.model.Client;
-import com.ecommerce.demo.util.Result;
 import io.vavr.control.Try;
 
 public interface ClientQueryRepository {
   Client findById(Long id);
-  Try<Result<Boolean>> exists(Long id);
+  Try<Boolean> exists(Long id);
 }
 
