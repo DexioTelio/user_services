e91b1170f0877e75559597f9f29a3d31b69fddd4
pepenacho <herreragrej@proton.me>
2024-12-11 22:23:00 -0300

    update ClientQueryRepositoryImpl

--- src/main/java/com/ecommerce/demo/repositories/ClientQueryRepositoryImpl.java
+++ src/main/java/com/ecommerce/demo/repositories/ClientQueryRepositoryImpl.java
@@ -1,14 +1,10 @@
 package com.ecommerce.demo.repositories;
 
-import com.ecommerce.demo.enums.DatabaseError;
 import com.ecommerce.demo.model.Client;
 import com.ecommerce.demo.repositories.interfaces.ClientQueryRepository;
-import com.ecommerce.demo.util.Result;
 import io.vavr.control.Try;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.Optional;
 
 public class ClientQueryRepositoryImpl implements ClientQueryRepository {
   private final JdbcTemplate jdbcTemplate;
@@ -24,17 +20,9 @@ public class ClientQueryRepositoryImpl implements ClientQueryRepository {
   }
 
   @Override
-  public Try<Result<Boolean>> exists(Long personId) {
+  public Try<Boolean> exists(Long personId) {
     String sql = " SELECT EXISTS (SELECT 1 FROM clients WHERE personId = ?";
 
-    return Try.of(() ->
-            Optional.ofNullable(
-                    jdbcTemplate.queryForObject(sql, new Object[]{personId}, Boolean.class)
-                    )
-                    .orElse(false)
-            )
-            .map(Result::success)
-            .recover(e -> Result.failure(
-                    DatabaseError.QUERY_EXECUTION_ERROR.getMessage() + ": " + e.getMessage()));
+    return Try.of(() -> jdbcTemplate.queryForObject(sql, new Object[]{personId}, Boolean.class));
   }
 }
