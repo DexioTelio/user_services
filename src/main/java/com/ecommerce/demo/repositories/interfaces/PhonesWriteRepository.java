b2874f03d4b773f992c848158b4700f8ad05bfb1
pepenacho <herreragrej@proton.me>
2024-12-13 22:28:56 -0300

    update PhonesWriteRepository

--- src/main/java/com/ecommerce/demo/repositories/interfaces/PhonesWriteRepository.java
+++ src/main/java/com/ecommerce/demo/repositories/interfaces/PhonesWriteRepository.java
@@ -1,9 +1,10 @@
 package com.ecommerce.demo.repositories.interfaces;
 
 import com.ecommerce.demo.dto.request.PhoneRequest;
+import com.ecommerce.demo.model.Phone;
 import com.ecommerce.demo.util.Result;
 
 public interface PhonesWriteRepository {
-    Result<Void> create(Long id, PhoneRequest request);
+    Result<Void> create(Phone phone);
     Result<String> findById(Long id);
 }
