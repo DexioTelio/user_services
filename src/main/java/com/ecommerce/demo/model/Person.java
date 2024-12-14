7d13714edbb225744fd6c4ea2c2d555e82249fbd
pepenacho <herreragrej@proton.me>
2024-12-13 22:28:25 -0300

    update Person

--- src/main/java/com/ecommerce/demo/model/Person.java
+++ src/main/java/com/ecommerce/demo/model/Person.java
@@ -3,14 +3,14 @@ package com.ecommerce.demo.model;
 import com.ecommerce.demo.enums.AccountStatus;
 import com.ecommerce.demo.enums.Gender;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 public class Person {
     private final Long id;
     private final String firstName;
     private final String lastName;
-    private final LocalDate dateOfBirth;
+    private final OffsetDateTime dateOfBirth;
     private final String email;
     private final String password;
     private final Gender gender;
@@ -47,7 +47,7 @@ public class Person {
         private Long id;
         private String firstName;
         private String lastName;
-        private LocalDate dateOfBirth;
+        private OffsetDateTime dateOfBirth;
         private String email;
         private String password;
         private Gender gender;
@@ -64,7 +64,7 @@ public class Person {
         public Builder setId(Long id) { this.id = id; return this; }
         public Builder setFirstName(String firstName) { this.firstName = firstName; return this; }
         public Builder setLastName(String lastName) { this.lastName = lastName; return this; }
-        public Builder setDateOfBirth(LocalDate dateOfBirth) { this.dateOfBirth = dateOfBirth; return this; }
+        public Builder setDateOfBirth(OffsetDateTime dateOfBirth) { this.dateOfBirth = dateOfBirth; return this; }
         public Builder setEmail(String email) { this.email = email; return this; }
         public Builder setPassword(String password) { this.password = password; return this; }
         public Builder setGender(Gender gender) { this.gender = gender; return this; }
@@ -86,7 +86,7 @@ public class Person {
     public Long getId() { return id; }
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
-    public LocalDate getDateOfBirth() { return dateOfBirth; }
+    public OffsetDateTime getDateOfBirth() { return dateOfBirth; }
     public String getEmail() { return email; }
     public String getPassword() { return password; }
     public Gender getGender() { return gender; }
