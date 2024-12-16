311990de141073bf3a928886ad61b4e33f81958a
pepenacho <herreragrej@proton.me>
2024-12-16 18:33:24 -0300

    update persons_roles

--- db/tables/persons_roles.sql
+++ db/tables/persons_roles.sql
@@ -1,4 +1,7 @@
 CREATE TABLE persons_roles (
-    person_id BIGINT PRIMARY KEY REFERENCES persons(id) ON DELETE CASCADE,
-    role_id BIGINT PRIMARY KEY REFERENCES roles(id) ON DELETE CASCADE,
-)
+    person_id BIGINT,
+    role_id BIGINT,
+    PRIMARY KEY (person_id, role_id),
+    FOREIGN KEY (person_id) REFERENCES persons(person_id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE
+);
