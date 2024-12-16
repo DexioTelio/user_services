5ef7f4f9e8ff43a3a3e375767faea390363adb6d
pepenacho <herreragrej@proton.me>
2024-12-16 17:56:01 -0300

    update clients

--- db/tables/client.sql
+++ db/tables/client.sql
@@ -3,13 +3,13 @@ CREATE TYPE customer_segment AS ENUM ('VIP', 'frequent', 'occasional', 'new');
 
 CREATE TABLE clients (
     client_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    person_id BIGINT REFERENCES persons(id) ON DELETE CASCADE,
+    person_id BIGINT REFERENCES persons(person_id) ON DELETE CASCADE,
     loyalty_points INT NOT NULL CHECK (loyalty_points >= 0),
     preferred_payment_method payment_method,
     customer_segment customer_segment,
-    last_purchase_date TIMESTAMP WITH TIME ZONE,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    update_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+    last_purchase_date TIMESTAMPTZ,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    update_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE INDEX idx_customer_segment ON clients(customer_segment);
