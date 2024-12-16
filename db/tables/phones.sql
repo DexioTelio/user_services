2e37c7f59d96023b2b6d22575dfcb0d682b9100b
pepenacho <herreragrej@proton.me>
2024-12-16 17:58:29 -0300

    update phones

--- db/tables/phones.sql
+++ db/tables/phones.sql
@@ -2,11 +2,11 @@ CREATE TYPE phone_type AS ENUM ('mobile', 'fixed', 'work', 'Emergency');
 
 CREATE TABLE phones (
     phone_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    person_id BIGINT NOT NULL REFERENCES person(id) ON DELETE CASCADE,
+    person_id BIGINT NOT NULL REFERENCES persons(person_id) ON DELETE CASCADE,
     phone_number VARCHAR(15) NOT NULL UNIQUE,
     phone_type phone_type,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    update_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    update_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE INDEX idx_phone_type ON phones USING btree (phone_type);
